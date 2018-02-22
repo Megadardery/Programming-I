ebcc04d236b3658fba9cf20f2739892794474689
Megadardery <Megadardery@yahoo.com>
2018-02-22 14:15:39 +0200

    cleanup and added Simple Substitution
    Added both Simple Substitution Cipher and Decipher.
    Only Railfence remains

--- Assignment 2/megadardery/all_ciphers.cpp
+++ Assignment 2/megadardery/all_ciphers.cpp
@@ -1,7 +1,8 @@
 /* FCI – Programming 1 – 2018 - Assignment 2
-  //WIP
-	Program Name: all_cipher.cpp
-	Last Modification Date: 21/02/2018
+
+	//WIP
+	Program Name: all_ciphers.cpp
+	Last Modification Date: 22/02/2018
 	Ahmed Nasr Eldardery (megadardery): G2 - 20170034
 	Purpose: This is a program that implements all 10 different types of ciphers in Assignment 2;
 */
@@ -9,15 +10,14 @@
 #include <string>
 using namespace std;
 
-#define NOT_AVAILABLE ""
-
+#define EMPTY ""
 const int c_morse_size = 64;
 
-const string c_morse[c_morse_size] = { " ", "-.-.--", ".-..-.", NOT_AVAILABLE, "...-..-", NOT_AVAILABLE, ".-...", ".----.", "-.--.", "-.--.-", NOT_AVAILABLE, ".-.-.", "--..--", "-....-", ".-.-.-", "-..-.", //	space !"#$%&'()*+,-./
+const string c_morse[c_morse_size] = { " ", "-.-.--", ".-..-.", EMPTY, "...-..-", EMPTY, ".-...", ".----.", "-.--.", "-.--.-", EMPTY, ".-.-.", "--..--", "-....-", ".-.-.-", "-..-.", //	space !"#$%&'()*+,-./
 "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.",	//	Numbers
-"---...", "-.-.-.", NOT_AVAILABLE, "-...-", NOT_AVAILABLE, "..--..", ".--.-.", //	:;<=>?@
+"---...", "-.-.-.", EMPTY, "-...-", EMPTY, "..--..", ".--.-.", //	:;<=>?@
 ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", //letters
-NOT_AVAILABLE, NOT_AVAILABLE, NOT_AVAILABLE, NOT_AVAILABLE, "..--.-" }; //	[\]^_
+EMPTY, EMPTY, EMPTY, EMPTY, "..--.-" }; //	[\]^_
 
 int getNumber(int begin, int end, string message) {
 	int answer;
@@ -41,7 +41,20 @@ int getNumber(int begin, int end, string message) {
 	return answer;
 }
 
+string getAlphabet() {
+	string charset;
+	while (true) {
+		cout << "\nEnter the cipher alphabet (26 letters). Be careful not to include repeated characters in the cipher alphabet: \n";
+		getline(cin, charset);
+		if (charset.length() != 26) {
+			cout << "Invalid input.\n";
+			continue;
+		}
+		return charset;
+	}
+}
 
+//--------------------------------------------------------------------------------------------------------------------------------------------
 string AffineCipher(string msg) {
 	string result = "";
 	for (int i = 0; i < (int)msg.length(); ++i) {
@@ -58,7 +71,7 @@ string AffineCipher(string msg) {
 	return result;
 }
 string CaesarCipher(string msg) {
-	int shifts = getNumber(0,25, "Enter the number of right shifts you want (0-25): \n");
+	int shifts = getNumber(0,25, "\nEnter the number of right shifts you want (0-25): \n");
 	string result = "";
 	for (int i = 0; i < (int)msg.length(); ++i) {
 		if (msg[i] >= 'A' && msg[i] <= 'Z')
@@ -96,7 +109,7 @@ string ROT13Cipher(string msg) {
 }
 string BaconianCipher(string msg) {
 
-	string answer = "";
+	string result = "";
 
 	for (int i = 0; i < (int)msg.length(); ++i) {
 		int ascii = msg[i];
@@ -105,7 +118,7 @@ string BaconianCipher(string msg) {
 			ascii -= 0x20;
 
 		if (ascii < 'A' || ascii > 'Z') {
-			answer += ascii;
+			result += ascii;
 			continue;
 		}
 
@@ -115,12 +128,22 @@ string BaconianCipher(string msg) {
 			charEncoding = "ab"[encoding & 1] + charEncoding;
 			encoding >>= 1;
 		}
-		answer = answer + charEncoding;
+		result = result + charEncoding;
 	}
-	return answer;
+	return result;
 }
 string SimpleSubstitutionCipher(string msg) {
-	throw new exception;
+	string charset = getAlphabet();
+	string result = "";
+	for (int i = 0; i < (int)msg.length(); ++i) {
+		if (msg[i] >= 'A' && msg[i] <= 'Z')
+			result += charset[msg[i] - 'A'];
+		else if (msg[i] >= 'a' && msg[i] <= 'z')
+			result += charset[msg[i] - 'a'];
+		else
+			result += msg[i];
+	}
+	return result;
 }
 string PolybiusSquareCipher(string msg) {
 	string result;
@@ -150,7 +173,7 @@ string MorseCipher(string msg) {
 	for (int i = 0; i < (int)msg.length(); ++i) {
 		if (msg[i] >= 'a' && msg[i] <= 'z')
 			msg[i] -= 0x20;
-		else if (msg[i]<' ' || msg[i] > '_' || c_morse[msg[i] - ' '] == NOT_AVAILABLE)
+		else if (msg[i]<' ' || msg[i] > '_' || c_morse[msg[i] - ' '] == EMPTY)
 			continue;
 
 		result += c_morse[msg[i] - ' '] + " ";
@@ -162,31 +185,31 @@ string MorseCipher(string msg) {
 string XORCipher(string msg) {
 	char secret;
 	while (true) {
-		cout << "Enter the secret key: \n";
+		cout << "\nEnter the secret key: \n";
 		string raw;
 		getline(cin, raw);
 		if (raw.length() != 1) {
-			cout << "Invalid input";
+			cout << "Invalid input.\n";
 			continue;
 		}
 		secret = raw[0];
 		break;
 	}
 	string hexa = "\n(Hexa: ";
-	string answer = "";
+	string result = "";
 	for (int i = 0; i < (int)msg.length(); ++i) {
 		int curr = msg[i] ^ secret;
 		hexa = (hexa + "0123456789ABCDEF"[curr % 16]) + "0123456789ABCDEF"[(curr >> 4) % 16];
-		answer += curr;
+		result += curr;
 	}
-	return answer + hexa + ")";
+	return result + hexa + ")";
 	
 	
 }
 string RailfenceCipher(string msg) {
 	throw new exception;
 }
-
+//--------------------------------------------------------------------------------------------------------------------------------------------
 string AffineDecipher(string msg) {
 	string result = "";
 	for (int i = 0; i < (int)msg.length(); ++i) {
@@ -203,7 +226,7 @@ string AffineDecipher(string msg) {
 	return result;
 }
 string CaesarDecipher(string msg) {
-	int shifts = getNumber(0, 25, "Enter the number of right shifts you had (0-25): \n");
+	int shifts = getNumber(0, 25, "\nEnter the number of right shifts you had (0-25): \n");
 	string result = "";
 	for (int i = 0; i < (int)msg.length(); ++i) {
 		if (msg[i] >= 'A' && msg[i] <= 'Z')
@@ -223,11 +246,11 @@ string ROT13Decipher(string msg) {
 }
 string BaconianDecipher(string msg) {
 
-	string answer = "";
+	string result = "";
 
 	for (int i = 4; i < (int)msg.length(); i += 5) {
 		if (msg[i - 4] != 'a' && msg[i - 4] != 'b') {
-			answer += msg[i - 4];
+			result += msg[i - 4];
 			i -= 4;
 			continue;
 
@@ -239,15 +262,29 @@ string BaconianDecipher(string msg) {
 			(msg[i - 1] - 'a') * 2 +
 			(msg[i] - 'a') * 1;
 
-		answer += (encoding + 'A');
+		result += (encoding + 'A');
 	}
-	return answer;
+	return result;
 }
 string SimpleSubstitutionDecipher(string msg) {
-	throw new exception;
+	string charset = getAlphabet();
+	string result = "";
+	for (int i = 0; i < (int)msg.length(); ++i) {
+		int j;
+		for (j = 0; j < 26; ++j) {
+			if (charset[j] == msg[i])
+				break;
+		}
+		if (j == 26)
+			result += msg[i];
+		else
+			result += j + 'a';
+	}
+	return result;
+
 }
 string PolybiusSquareDecipher(string msg) {
-	string answer = "";
+	string result = "";
 
 	int first = -1;
 
@@ -259,14 +296,14 @@ string PolybiusSquareDecipher(string msg) {
 				int code = 5 * first + msg[i] - '0' - 1;
 				if (code >= 22)
 					++code;
-				answer += code + 'A';
+				result += code + 'A';
 				first = -1;
 			}
 		}
 		else
-			answer += msg[i];
+			result += msg[i];
 	}
-	return answer;
+	return result;
 }
 string MorseDecipher(string msg) {
 	string currLetter = "";
@@ -298,19 +335,17 @@ string XORDecipher(string msg) {
 string RailfenceDecipher(string msg) {
 	throw new exception;
 }
+//--------------------------------------------------------------------------------------------------------------------------------------------
 
 int main() {
 
-	string(*ciphers[10])(string) = { AffineCipher,CaesarCipher,AtbashCipher,ROT13Cipher,BaconianCipher,SimpleSubstitutionCipher,PolybiusSquareCipher,MorseCipher,XORCipher,RailfenceCipher};
+	string(*ciphers[10])(string) = { AffineCipher,CaesarCipher,AtbashCipher,ROT13Cipher,BaconianCipher,SimpleSubstitutionCipher,PolybiusSquareCipher,MorseCipher,XORCipher,RailfenceCipher };
 	string(*deciphers[10])(string) = { AffineDecipher,CaesarDecipher,AtbashDecipher,ROT13Decipher,BaconianDecipher,SimpleSubstitutionDecipher,PolybiusSquareDecipher,MorseDecipher,XORDecipher,RailfenceDecipher };
 
-
-
-	//for (char x = cin.get(); x != '\n'; x = cin.get()) cout << char(((x - 'A') * 5 + 8) % 26 + 'A');
 	cout << "Ahlan ya user ya habibi.\n";
 
 	while (true) {
-		cout << "What do you like to do today?\n" 
+		cout << "\nWhat do you like to do today?\n" 
 			<< "1- Cipher a message\n"
 			<< "2- Decipher a message\n"
 			<< "3- End\n";
@@ -319,24 +354,24 @@ int main() {
 		getline(cin, action);
 		
 		if (action == "3") {
-			cout << "Habibi ya user, ashofak later :)\n";
+			cout << "\nHabibi ya user, ashofak later :)\n";
 			break;
 		}
 		else if (action != "2" && action != "1") {
-			cout << "Invalid input\n";
+			cout << "Invalid input.\n";
 			continue;
 		}
 
-		int mycipher = getNumber(0, 9, "Which encryption algorithm do you want to try? (0-9)\n");
+		int mycipher = getNumber(0, 9, "\nWhich encryption algorithm do you want to try? (0-9)\n");
 
 		if (action == "1") {
-			cout << "Please enter the message to cipher:\n";
+			cout << "\nPlease enter the message to cipher:\n";
 			string message;
 			getline(cin, message);
 			cout << ciphers[mycipher](message) << "\n";
 		}
 		else if (action == "2") {
-			cout << "Please enter the message to decipher:\n";
+			cout << "\nPlease enter the message to decipher:\n";
 			string message;
 			getline(cin, message);
 			cout << deciphers[mycipher](message) << "\n";
